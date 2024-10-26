303b11dad316e429d5c806ca289404663d5f0b11
parmeshwar-1234 <avhadparmeshwar2122@ternaengg.ac.in>
2024-10-26 11:52:58 +0530

    yes

--- news_board.php
+++ news_board.php
@@ -126,6 +126,9 @@ $apiResult = fetchLatestNews();
             /* Pause on hover */
         }
     </style>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css"
+        integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
 </head>
 
 <body>
